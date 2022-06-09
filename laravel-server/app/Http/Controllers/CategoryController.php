68e8809ec624931ebf19603908a4c7404a21ff96
MJoe111 <joemoussally111@gmail.com>
2022-06-10 00:01:40 +0300

    Adding CategotyController apis

--- laravel-server/app/Http/Controllers/CategoryController.php
+++ laravel-server/app/Http/Controllers/CategoryController.php
@@ -3,8 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\category;
 
 class CategoryController extends Controller
 {
-    //
+    // add a clothe category
+    public function addCategory(Request $Request) {
+
+        $category_name = $Request->category_name;
+
+        //check if category already exists
+        if (Category::where('category',$category_name)->exists()) {
+            return response()->json([
+                'status' => 'exists',
+            ],200);
+        }
+
+        $category = new Category;
+        $category->category = $category_name;
+        $category->save();
+
+        return response()->json([
+            'status' => 'added',
+            'category' => $category_name
+        ],200);
+    }
+
+    // remove a clothe category
+    public function removeCategory(Request $Request) {
+        $category_name = $Request->category_name;
+
+        $product_id = $Request->product_id;
+
+        $category = Category::where('category',$category_name)->first();
+        $category->delete();
+
+        return response()->json([
+            'status' => 'deleted'
+        ],200);
+    }
 }
--- laravel-server/routes/api.php
+++ laravel-server/routes/api.php
@@ -4,22 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JWTController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
+//JWT apis
 Route::group(['middleware' => 'api'], function($router) {
     Route::post('/register', [JWTController::class, 'register']);
     Route::post('/login', [JWTController::class, 'login']);
@@ -28,13 +16,7 @@ Route::group(['middleware' => 'api'], function($router) {
     Route::post('/profile', [JWTController::class, 'profile']);
 });
 
-// Route::group(['prefix' => 'admin'], function(){
-//     Route::group(['middleware' => 'role.user'], function(){
-//         Route::get('/', [AdminController::class, 'getAllSalaries']);
-//         Route::get('/salaries', [AdminController::class, 'getAllSalaries']);
-//     });
-// });
-
+//admin apis
 Route::post('/add_product',[ProductController::class, 'addProduct']);
 Route::post('/remove_product',[ProductController::class, 'removeProduct']);
 Route::post('/edit_quantity',[ProductController::class, 'editQuantity']);
@@ -42,3 +24,7 @@ Route::post('/edit_name',[ProductController::class, 'editName']);
 Route::post('/edit_price',[ProductController::class, 'editPrice']);
 Route::post('/edit_picture',[ProductController::class, 'editPicture']);
 Route::post('/edit_category',[ProductController::class, 'editCategory']);
+
+//category apis
+Route::post('/add_category',[CategoryController::class, 'addCategory']);
+Route::post('/remove_category',[CategoryController::class, 'removeCategory']);
