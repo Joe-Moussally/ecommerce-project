4f40fbc0c548b3e78fd3ab033c858598d1bd80de
MJoe111 <joemoussally111@gmail.com>
2022-06-09 19:21:53 +0300

    Fixing JWT TOKEN CONTROLLER

--- laravel-server/app/Models/User.php
+++ laravel-server/app/Models/User.php
@@ -6,41 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 
     /**
      * Get the identifier that will be stored in the subject claim of the JWT.
@@ -61,5 +31,4 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
-
 }
