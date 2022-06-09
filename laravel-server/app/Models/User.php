41f322249ca61b0736f0aa173c7f688887b4d4a4
MJoe111 <joemoussally111@gmail.com>
2022-06-09 19:47:54 +0300

    Adding laravel server folder

--- laravel-server/app/Models/User.php
+++ laravel-server/app/Models/User.php
@@ -6,29 +6,39 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Tymon\JWTAuth\Contracts\JWTSubject;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * Get the identifier that will be stored in the subject claim of the JWT.
+     * The attributes that are mass assignable.
      *
-     * @return mixed
+     * @var array<int, string>
      */
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
 
     /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
+     * The attributes that should be hidden for serialization.
      *
-     * @return array
+     * @var array<int, string>
      */
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
