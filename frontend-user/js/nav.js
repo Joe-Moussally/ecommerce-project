d66a9ee365c48f5ec757cae0e59b8b3188167561
MJoe111 <joemoussally111@gmail.com>
2022-06-12 00:46:43 +0300

    Adding home html

--- frontend-user/js/nav.js
+++ frontend-user/js/nav.js
@@ -1,6 +1,21 @@
 let loginButton = document.getElementById('login-nav');
 let signUpButton = document.getElementById('signup-nav');
 
+//check if user is logged in
+user_id = localStorage.getItem('user_id');
+console.log(user_id);
+if (user_id == null || user_id == 0) {
+    document.getElementById('profile-nav').style.display = 'none';
+    document.getElementById('login-nav').style.display = 'block';
+    document.getElementById('signup-nav').style.display = 'block';
+    document.getElementById('seperator').style.display = 'block';
+} else {
+    document.getElementById('profile-nav').style.display = 'block';
+    document.getElementById('login-nav').style.display = 'none';
+    document.getElementById('signup-nav').style.display = 'none';
+    document.getElementById('seperator').style.display = 'none';
+}
+
 //adding event listeners to nav buttons
 loginButton.addEventListener('click', () => {
     window.location.replace('./login.html')
