6ac97860e7c9d7bfa162681ad73a02ec545cad5d
MJoe111 <joemoussally111@gmail.com>
2022-06-12 01:04:32 +0300

    Adding more responsiveness

--- frontend-user/js/home.js
+++ frontend-user/js/home.js
@@ -7,3 +7,9 @@ if (user_id == null || user_id == 0) {
 }
 //--------------------------------------
 
+// const toBase64 = file => new Promise((resolve, reject) => {
+//     const reader = new FileReader();
+//     reader.readAsDataURL(file);
+//     reader.onload = () => resolve(reader.result);
+//     reader.onerror = error => reject(error);
+// });
