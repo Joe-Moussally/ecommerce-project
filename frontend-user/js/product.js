91d4b050fcc84902124f84d18407d89ad8e7e96f
MJoe111 <joemoussally111@gmail.com>
2022-06-12 03:13:05 +0300

    Adding allow-cross-origin to Laravel-server

--- frontend-user/js/product.js
+++ frontend-user/js/product.js
@@ -1,9 +1,9 @@
 let product_id = localStorage.getItem('product_id');
-let url = 'http://127.0.0.1:8000/products/'+product_id;
+let url = 'http://127.0.0.1:8000/api/products/'+product_id;
 
 axios({
     method: 'POST',
     url: url
 }).then((Response)=> {
-    console.log(Response.data)
+    console.log(Response.data.product)
 })
