481b36ed6b575a06e42edf9b4f2baa92db946894
MJoe111 <joemoussally111@gmail.com>
2022-06-13 01:32:16 +0300

    Adding logout

--- electron-admin/js/add_product.js
+++ electron-admin/js/add_product.js
@@ -1,10 +1,5 @@
 let submit = document.getElementById('submit');
 
-
-
-// const uploadedFile = document.querySelector('#photo').files[0];
-// toBase64(uploadedFile)
-
 //function to turn image to base64
 const toBase64 = file => new Promise((resolve, reject) => {
     const reader = new FileReader();
@@ -13,8 +8,6 @@ const toBase64 = file => new Promise((resolve, reject) => {
     reader.onerror = error => reject(error);
 });
 
-
-
 submit.addEventListener('click', () => {
 
     let name = document.getElementById('name');
@@ -22,29 +15,31 @@ submit.addEventListener('click', () => {
     let category = document.getElementById('category');
     let quantity = document.getElementById('quantity');
     let uploadedFile = document.getElementById('img').files[0];
-    toBase64(uploadedFile);
+    toBase64(uploadedFile)
+    .then(res => {
 
-    //header
-    const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + localStorage.getItem('token')
-      }
+        //header
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+        }
 
-    //formData
-    let data = new FormData();
-    data.append('name',name.value)
-    data.append('price',price.value)
-    data.append('quantity',quantity.value)
-    data.append('category_id',category.value)
-    data.append('picture',uploadedFile)
+        //formData
+        let data = new FormData();
+        data.append('name',name.value)
+        data.append('price',price.value)
+        data.append('quantity',quantity.value)
+        data.append('category_id',category.value)
+        data.append('picture',res)
 
-    axios({
-        headers: headers,
-        method: 'POST',
-        url: 'http://127.0.0.1:8000/api/add_product',
-        data:data
-    }).then((Response) => {
-        console.log(Response.data)
+        axios({
+            headers: headers,
+            method: 'POST',
+            url: 'http://127.0.0.1:8000/api/add_product',
+            data:data
+        }).then((Response) => {
+            console.log(Response.data)
+            window.location.replace('./home.html')
+        })
     })
-
 })
--- electron-admin/js/home.js
+++ electron-admin/js/home.js
@@ -5,3 +5,24 @@ let search_product = document.getElementById('search-product');
 add_product.addEventListener('click', () => {
     window.location.replace('./add_product.html');
 })
+
+//adding logout functionality
+document.getElementById('admin-logout').addEventListener('click', () => {
+
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+    }
+
+    axios({
+        headers: headers,
+        method: 'POST',
+        url: 'http://127.0.0.1:8000/api/logout'
+    }).then((Response) => {
+        console.log(Response)
+        localStorage.setItem('user_id',0);
+        localStorage.setItem('token',null);
+        window.location.replace('./login.html');
+    })
+    
+})
