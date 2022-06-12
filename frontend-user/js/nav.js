a25d90e5a2e2c455aaf7c109cf08fe706bb3563b
MJoe111 <joemoussally111@gmail.com>
2022-06-13 02:33:13 +0300

    Adding logout to nav

--- frontend-user/js/nav.js
+++ frontend-user/js/nav.js
@@ -8,9 +8,11 @@ if (user_id == null || user_id == 0) {
     document.getElementById('profile-nav').style.display = 'none';
     document.getElementById('login-nav').style.display = 'block';
     document.getElementById('signup-nav').style.display = 'block';
+    document.getElementById('logout-nav').style.display = 'none';
     document.getElementById('seperator').style.display = 'block';
 } else {
     document.getElementById('profile-nav').style.display = 'block';
+    document.getElementById('logout-nav').style.display = 'block';
     document.getElementById('login-nav').style.display = 'none';
     document.getElementById('signup-nav').style.display = 'none';
     document.getElementById('seperator').style.display = 'none';
@@ -23,4 +25,23 @@ loginButton.addEventListener('click', () => {
 
 signUpButton.addEventListener('click', () => {
     window.location.replace('./signup.html')
+})
+
+//adding logout functionality
+document.getElementById('logout-nav').addEventListener('click', () => {
+
+    //header
+    const headers = {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+    }
+
+    axios({
+        headers: headers,
+        method:'POST',
+        url: 'http://127.0.0.1:8000/api/logout'
+    }).then((Response) => {
+        localStorage.setItem('user_id',0)
+        window.location.replace('./home.html')
+    })
 })
